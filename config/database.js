6e3e164a9723ddc6c490e534056aa6eb41aeb4a0
metaliccode <isan.mh69@gmail.com>
2023-03-14 14:58:19 +0700

    Add: Koneksi ke database posgresql

--- config/database.js
+++ config/database.js
@@ -6,6 +6,13 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    // untuk postgresql
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
   test: {
     username: "root",
@@ -22,3 +29,18 @@ module.exports = {
     dialect: "mysql",
   },
 };
+
+// postgresql
+// module.exports = {
+//   HOST: "localhost" || process.env.DB_HOST,
+//   USER: "postgres" || process.env.DB_USERNAME,
+//   PASSWORD: process.env.DB_PASSWORD,
+//   DB: process.env.DB_NAME,
+//   dialect: "postgres" || process.env.DB_DIALECT,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// };
