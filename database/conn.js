82ab33fa78d17b4a7e8082d90b7123eabf099a65
metaliccode <isan.mh69@gmail.com>
2023-03-14 08:41:42 +0700

    Perbaikan Koneksi DB mysql

--- database/conn.js
+++ database/conn.js
@@ -1,17 +1,17 @@
-const mysql = require("mysql");
-require("dotenv").config();
+// const mysql = require("mysql");
+// require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: "localhost" || process.env.DB_HOST,
-  user: "root" || process.env.DB_USERNAME,
-  password: "" || process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const connection = mysql.createConnection({
+//   host: "localhost" || process.env.DB_HOST,
+//   user: "root" || process.env.DB_USERNAME,
+//   password: "" || process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
-// cek koneksi database
-connection.connect((err) => {
-  if (err) throw err;
-  console.log("Database terkoneksi");
-});
+// // cek koneksi database
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log("Database terkoneksi");
+// });
 
-module.exports = connection;
+// module.exports = connection;
--- models/contactModel.js
+++ models/contactModel.js
@@ -1,25 +1,25 @@
-// model query mysql
-module.exports = {
-  fetchData: (db, callback) => {
-    db.query("SELECT * FROM contacts", callback);
-  },
-  getById: (db, id, callback) => {
-    db.query("SELECT * FROM contacts WHERE id = ?", [id], callback);
-  },
-  insertData: (db, data, callback) => {
-    db.query("INSERT INTO contacts SET ?", [data], callback);
-  },
-  updateData: (db, data, id, callback) => {
-    db.query("UPDATE contacts SET ? WHERE id =? ", [data, id], callback);
-  },
-  deleteData: (db, id, callback) => {
-    db.query("DELETE FROM contacts WHERE id = ? ", [id], callback);
-  },
-  search: (db, keyword, callback) => {
-    db.query(
-      "SELECT * FROM contacts WHERE name LIKE ? OR phone LIKE ?",
-      [`%${keyword}%`, `%${keyword}%`],
-      callback
-    );
-  },
-};
+// // model query mysql
+// module.exports = {
+//   fetchData: (db, callback) => {
+//     db.query("SELECT * FROM contacts", callback);
+//   },
+//   getById: (db, id, callback) => {
+//     db.query("SELECT * FROM contacts WHERE id = ?", [id], callback);
+//   },
+//   insertData: (db, data, callback) => {
+//     db.query("INSERT INTO contacts SET ?", [data], callback);
+//   },
+//   updateData: (db, data, id, callback) => {
+//     db.query("UPDATE contacts SET ? WHERE id =? ", [data, id], callback);
+//   },
+//   deleteData: (db, id, callback) => {
+//     db.query("DELETE FROM contacts WHERE id = ? ", [id], callback);
+//   },
+//   search: (db, keyword, callback) => {
+//     db.query(
+//       "SELECT * FROM contacts WHERE name LIKE ? OR phone LIKE ?",
+//       [`%${keyword}%`, `%${keyword}%`],
+//       callback
+//     );
+//   },
+// };
