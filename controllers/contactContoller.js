ce7da998da5347e89b5adbc869f5239a539f2927
Isanz mh <112740406+isanmh@users.noreply.github.com>
2023-08-28 22:26:06 +0700

    Update contactContoller.js

--- controllers/contactContoller.js
+++ controllers/contactContoller.js
@@ -21,32 +21,32 @@ module.exports = {
       layout: "layouts/main-layout",
     });
   },
-  store: (req, res) => {
-    const errors = validationResult(req);
-    const { name, email, phone } = req.body;
-    const data = {
-      name,
-      email,
-      phone,
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-    // logika jika ada error
-    if (!errors.isEmpty()) {
-      //   return res.status(422).json({ errors: errors.array() });
-      res.render("contacts/create", {
-        layout: "layouts/main-layout",
-        errors: errors.array(),
-        old: data,
-      });
-    } else {
-      Contact.insertData(req.db, data, (err, result) => {
-        if (err) throw err;
-        req.flash("success", "Data berhasil ditambahkan");
-        res.redirect("/contacts");
-      });
-    }
-  },
+  // store: (req, res) => {
+  //   const errors = validationResult(req);
+  //   const { name, email, phone } = req.body;
+  //   const data = {
+  //     name,
+  //     email,
+  //     phone,
+  //     created_at: new Date(),
+  //     updated_at: new Date(),
+  //   };
+  //   // logika jika ada error
+  //   if (!errors.isEmpty()) {
+  //     //   return res.status(422).json({ errors: errors.array() });
+  //     res.render("contacts/create", {
+  //       layout: "layouts/main-layout",
+  //       errors: errors.array(),
+  //       old: data,
+  //     });
+  //   } else {
+  //     Contact.insertData(req.db, data, (err, result) => {
+  //       if (err) throw err;
+  //       req.flash("success", "Data berhasil ditambahkan");
+  //       res.redirect("/contacts");
+  //     });
+  //   }
+  // },
   // edit: (req, res) => {
   //   const id = req.params.id;
   //   Contact.getById(req.db, id, (err, result) => {
@@ -58,38 +58,38 @@ module.exports = {
   //     });
   //   });
   // },
-  update: (req, res) => {
-    const { id, name, email, phone } = req.body;
-    const data = {
-      name,
-      email,
-      phone,
-    };
-    Contact.updateData(req.db, data, id, (err, result) => {
-      if (err) throw err;
-      req.flash("success", "Data berhasil diubah");
-      res.redirect("/contacts");
-    });
-  },
-  destroy: (req, res) => {
-    const id = req.params.id;
-    Contact.deleteData(req.db, id, (err, result) => {
-      if (err) throw err;
-      req.flash("success", "Data berhasil dihapus");
-      res.redirect("/contacts");
-    });
-  },
-  search: (req, res) => {
-    const keyword = req.query.keyword;
-    Contact.search(req.db, keyword, (err, result) => {
-      if (err) throw err;
-      res.render("contacts/index", {
-        judul: "Contact",
-        data: result,
-        layout: "layouts/main-layout",
-      });
-    });
-  },
+  // update: (req, res) => {
+  //   const { id, name, email, phone } = req.body;
+  //   const data = {
+  //     name,
+  //     email,
+  //     phone,
+  //   };
+  //   Contact.updateData(req.db, data, id, (err, result) => {
+  //     if (err) throw err;
+  //     req.flash("success", "Data berhasil diubah");
+  //     res.redirect("/contacts");
+  //   });
+  // },
+  // destroy: (req, res) => {
+  //   const id = req.params.id;
+  //   Contact.deleteData(req.db, id, (err, result) => {
+  //     if (err) throw err;
+  //     req.flash("success", "Data berhasil dihapus");
+  //     res.redirect("/contacts");
+  //   });
+  // },
+  // search: (req, res) => {
+  //   const keyword = req.query.keyword;
+  //   Contact.search(req.db, keyword, (err, result) => {
+  //     if (err) throw err;
+  //     res.render("contacts/index", {
+  //       judul: "Contact",
+  //       data: result,
+  //       layout: "layouts/main-layout",
+  //     });
+  //   });
+  // },
 
   // menggunakan sequelize orm
   index: (req, res) => {
@@ -104,46 +104,46 @@ module.exports = {
       });
     });
   },
-//   store: (req, res) => {
-//     // console.log(req.file);
-//     const errors = validationResult(req);
-//     const { name, email, phone } = req.body;
-//     const img = req.file;
-//     if (req.file) {
-//       var image = req.file.filename;
-//     } else {
-//       var image = null;
-//     }
-//     const data = {
-//       name,
-//       email,
-//       phone,
-//       image,
-//     };
-//     // logika jika ada error
-//     if (!errors.isEmpty()) {
-//       res.render("contacts/create", {
-//         layout: "layouts/main-layout",
-//         errors: errors.array(),
-//         old: data,
-//       });
-//     } else {
-//       const contact = db.Contact.create({
-//         name: name,
-//         email: email,
-//         phone: phone,
-//         image: image,
-//       });
-//       contact
-//         .then((result) => {
-//           req.flash("success", "Data berhasil ditambahkan");
-//           res.redirect("/contacts");
-//         })
-//         .catch((err) => {
-//           throw err;
-//         });
-//     }
-//   },
+  store: (req, res) => {
+    // console.log(req.file);
+    const errors = validationResult(req);
+    const { name, email, phone } = req.body;
+    const img = req.file;
+    if (req.file) {
+      var image = req.file.filename;
+    } else {
+      var image = null;
+    }
+    const data = {
+      name,
+      email,
+      phone,
+      image,
+    };
+    // logika jika ada error
+    if (!errors.isEmpty()) {
+      res.render("contacts/create", {
+        layout: "layouts/main-layout",
+        errors: errors.array(),
+        old: data,
+      });
+    } else {
+      const contact = db.Contact.create({
+        name: name,
+        email: email,
+        phone: phone,
+        image: image,
+      });
+      contact
+        .then((result) => {
+          req.flash("success", "Data berhasil ditambahkan");
+          res.redirect("/contacts");
+        })
+        .catch((err) => {
+          throw err;
+        });
+    }
+  },
   edit: (req, res) => {
     const id = req.params.id;
     db.Contact.findByPk(id).then((contact) => {
@@ -154,84 +154,84 @@ module.exports = {
       });
     });
   },
-//   update: (req, res) => {
-//     const { id, name, email, phone } = req.body;
-//     db.Contact.findByPk(id).then((contact) => {
-//       // jika ada file
-//       if (req.file) {
-//         if (contact.image !== null) {
-//           const filepath = `./public/images/${contact.image}`;
-//           fs.unlinkSync(filepath);
-//         }
-//         var image = req.file.filename;
-//       } else {
-//         var image = contact.image;
-//       }
-//       db.Contact.update(
-//         {
-//           name: name,
-//           email: email,
-//           phone: phone,
-//           image: image,
-//         },
-//         {
-//           where: {
-//             id: id,
-//           },
-//         }
-//       ).then((result) => {
-//         req.flash("success", "Data berhasil diubah");
-//         res.redirect("/contacts");
-//       });
-//     });
-//   },
-//   destroy: (req, res) => {
-//     const id = req.params.id;
-//     db.Contact.findByPk(id).then((contact) => {
-//       if (contact.image !== null) {
-//         const filepath = `./public/images/${contact.image}`;
-//         fs.unlinkSync(filepath);
-//         // fs.unlink(filepath, (err) => {
-//         //   if (err) throw err;
-//         // });
-//       }
-//       db.Contact.destroy({
-//         where: {
-//           id: id,
-//         },
-//         // jika ada file
-//       }).then((contact) => {
-//         req.flash("success", "Data berhasil dihapus");
-//         res.redirect("/contacts");
-//       });
-//     });
-//   },
-//   search: (req, res) => {
-//     const keyword = req.query.keyword;
-//     db.Contact.findAll({
-//       where: {
-//         // name: {
-//         //   [Op.like]: "%" + keyword + "%",
-//         // },
-//         [Op.or]: [
-//           {
-//             name: {
-//               [Op.like]: `%${keyword}%`,
-//             },
-//           },
-//           {
-//             phone: {
-//               [Op.like]: `%${keyword}%`,
-//             },
-//           },
-//         ],
-//       },
-//     }).then((contacts) => {
-//       res.render("contacts/index", {
-//         judul: "Contact",
-//         data: contacts,
-//         layout: "layouts/main-layout",
-//       });
-//     });
-//   },
+  update: (req, res) => {
+    const { id, name, email, phone } = req.body;
+    db.Contact.findByPk(id).then((contact) => {
+      // jika ada file
+      if (req.file) {
+        if (contact.image !== null) {
+          const filepath = `./public/images/${contact.image}`;
+          fs.unlinkSync(filepath);
+        }
+        var image = req.file.filename;
+      } else {
+        var image = contact.image;
+      }
+      db.Contact.update(
+        {
+          name: name,
+          email: email,
+          phone: phone,
+          image: image,
+        },
+        {
+          where: {
+            id: id,
+          },
+        }
+      ).then((result) => {
+        req.flash("success", "Data berhasil diubah");
+        res.redirect("/contacts");
+      });
+    });
+  },
+  destroy: (req, res) => {
+    const id = req.params.id;
+    db.Contact.findByPk(id).then((contact) => {
+      if (contact.image !== null) {
+        const filepath = `./public/images/${contact.image}`;
+        fs.unlinkSync(filepath);
+        // fs.unlink(filepath, (err) => {
+        //   if (err) throw err;
+        // });
+      }
+      db.Contact.destroy({
+        where: {
+          id: id,
+        },
+        // jika ada file
+      }).then((contact) => {
+        req.flash("success", "Data berhasil dihapus");
+        res.redirect("/contacts");
+      });
+    });
+  },
+  search: (req, res) => {
+    const keyword = req.query.keyword;
+    db.Contact.findAll({
+      where: {
+        // name: {
+        //   [Op.like]: "%" + keyword + "%",
+        // },
+        [Op.or]: [
+          {
+            name: {
+              [Op.like]: `%${keyword}%`,
+            },
+          },
+          {
+            phone: {
+              [Op.like]: `%${keyword}%`,
+            },
+          },
+        ],
+      },
+    }).then((contacts) => {
+      res.render("contacts/index", {
+        judul: "Contact",
+        data: contacts,
+        layout: "layouts/main-layout",
+      });
+    });
+  },
 };
