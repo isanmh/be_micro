3e4c7d616968d860e8f68398302bf373533149ed
Isanz mh <112740406+isanmh@users.noreply.github.com>
2023-08-28 22:04:52 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,46 +1,27 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-    // untuk postgresql
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
   },
 };
-
-// postgresql
-// module.exports = {
-//   HOST: "localhost" || process.env.DB_HOST,
-//   USER: "postgres" || process.env.DB_USERNAME,
-//   PASSWORD: process.env.DB_PASSWORD,
-//   DB: process.env.DB_NAME,
-//   dialect: "postgres" || process.env.DB_DIALECT,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
