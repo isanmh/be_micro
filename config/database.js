ca9b2758daeaa5e67105bd98816adb35de2f074f
isanmh <115705004@student.uinsgd.com>
2024-09-25 22:16:29 +0700

    fix: Connection to database Mysql

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
